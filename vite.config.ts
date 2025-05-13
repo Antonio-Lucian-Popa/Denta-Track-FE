21a73d4a91b2a7f9278db243b0c5a13a4fbd2b7e
antonio <antonio.popa.dev@gmail.com>
2025-05-13 09:15:06 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/dentatrack-fe/', // Set your base path here
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
